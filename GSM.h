4c21203e170a9f402584f860c3f1aac24cb04c66
Per Åstrand <per.astrand@gmail.com>
2013-11-03 12:29:48 +0100

    Changed pins for POWER on RESET to reflect board

--- GSM.h
+++ GSM.h
@@ -41,8 +41,8 @@
 
 
 // pins definition
-#define GSM_ON              8 // connect GSM Module turn ON to pin 77 
-#define GSM_RESET           9 // connect GSM Module RESET to pin 35
+#define GSM_ON              6 // connect GSM Module turn ON to pin 77 
+#define GSM_RESET           5 // connect GSM Module RESET to pin 35
 //#define DTMF_OUTPUT_ENABLE  71 // connect DTMF Output Enable not used
 #define DTMF_DATA_VALID     14 // connect DTMF Data Valid to pin 14
 #define DTMF_DATA0          72 // connect DTMF Data0 to pin 72
